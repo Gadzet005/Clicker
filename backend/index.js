2ebaa433ddf5772510141596922447b93b0f7272
Mar-Ml <vasa11111112.qaz@gmail.com>
2024-07-12 11:26:48 +0300

    fixed backend libraries

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 const app = express();
 
+const urlencodedParser = express.urlencoded({extended: false});
+
 app.get("/", (req, res) => {
     res.json({
         hello: "hello world!"
@@ -15,5 +17,11 @@ async function start() {
         console.log(`Listening on port ${port}`);
     });
 }
+
+app.post("/", urlencodedParser, function (request, response) { // register
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.send(`${request.body.userName} - ${request.body.userAge}`);
+});
 
 start();
